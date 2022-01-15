c7e7f6861d283e67ed54d2cdac1ccf80abe17f8e
Vladislav Lysak <vladislavlysak@MacBook-Air-Vladislav.local>
2022-01-15 23:14:22 +0200

    Added regular expressions

--- main_page/signals.py
+++ main_page/signals.py
@@ -1,11 +1,11 @@
 from django.db.models.signals import pre_save
-
 from .models import TheUsers
+import re
 
 
 def pre_save_signal_the_users(sender, **kwargs):
-    name = kwargs['instance'].name
-    surname = kwargs['instance'].surname
+    name = re.sub("[^A-Za-z ]", "", kwargs['instance'].name).strip()
+    surname = re.sub("[^A-Za-z ]", "", kwargs['instance'].surname).strip()
     kwargs['instance'].name = str(name).capitalize()
     kwargs['instance'].surname = str(surname).capitalize()
 pre_save.connect(pre_save_signal_the_users, sender=TheUsers)
